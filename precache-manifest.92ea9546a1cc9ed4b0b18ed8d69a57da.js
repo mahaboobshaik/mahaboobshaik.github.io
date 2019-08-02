98c287a16286672e38f1514ca1b5af3c7d34471e
mahaboob Basha Shaik <mahaboob.basha@dolcera.com>
2019-08-02 10:48:30 +0530

    added basic google authentication

---
+++ precache-manifest.92ea9546a1cc9ed4b0b18ed8d69a57da.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b558f754b4b299d1de84a8167b994c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "887a8b5149cc86ee257f",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "46504c8aa1e9c689a6be",
+    "url": "/static/css/5.7b68e035.chunk.css"
+  },
+  {
+    "revision": "cb1716dccc825ae17826",
+    "url": "/static/css/main.22825f5d.chunk.css"
+  },
+  {
+    "revision": "e88d8fed133ca249caee",
+    "url": "/static/js/10.c466effe.chunk.js"
+  },
+  {
+    "revision": "887a8b5149cc86ee257f",
+    "url": "/static/js/2.6e2b3555.chunk.js"
+  },
+  {
+    "revision": "0f17ff39addd267ade8a",
+    "url": "/static/js/3.f773f883.chunk.js"
+  },
+  {
+    "revision": "a5b3923f55112a9cedfe",
+    "url": "/static/js/4.75712c4c.chunk.js"
+  },
+  {
+    "revision": "46504c8aa1e9c689a6be",
+    "url": "/static/js/5.e4d96f2f.chunk.js"
+  },
+  {
+    "revision": "8217494eb324df3f4eaa",
+    "url": "/static/js/6.4097aa61.chunk.js"
+  },
+  {
+    "revision": "3390e49a23602b28277c",
+    "url": "/static/js/7.bc6617f7.chunk.js"
+  },
+  {
+    "revision": "d96ed0bcd3ec00d1ea72",
+    "url": "/static/js/8.6bdca5b1.chunk.js"
+  },
+  {
+    "revision": "afcc3c9e45d5fbdc5bb9",
+    "url": "/static/js/9.8c097754.chunk.js"
+  },
+  {
+    "revision": "cb1716dccc825ae17826",
+    "url": "/static/js/main.12e60a87.chunk.js"
+  },
+  {
+    "revision": "5c8eb3f3772168c7c757",
+    "url": "/static/js/runtime~main.f55c2e7a.js"
+  },
+  {
+    "revision": "d329cc8b34667f114a95422aaad1b063",
+    "url": "/static/media/Roboto-Bold.d329cc8b.ttf"
+  },
+  {
+    "revision": "7b5fb88f12bec8143f00e21bc3222124",
+    "url": "/static/media/Roboto-Light.7b5fb88f.ttf"
+  },
+  {
+    "revision": "fe13e4170719c2fc586501e777bde143",
+    "url": "/static/media/Roboto-Medium.fe13e417.ttf"
+  },
+  {
+    "revision": "ac3f799d5bbaf5196fab15ab8de8431c",
+    "url": "/static/media/Roboto-Regular.ac3f799d.ttf"
+  },
+  {
+    "revision": "e275bf6b681fd7108a7ddcd22b640097",
+    "url": "/static/media/loading.e275bf6b.gif"
+  }
+]);
---
+++ static/js/7.bc6617f7.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{87:function(n,e,t){"use strict";t.r(e);var a=t(5),c=t(6),o=t(8),r=t(7),u=t(9),i=t(0),s=t.n(i),p=function(n){function e(){return Object(a.a)(this,e),Object(o.a)(this,Object(r.a)(e).apply(this,arguments))}return Object(u.a)(e,n),Object(c.a)(e,[{key:"render",value:function(){return s.a.createElement("div",null," Downlaods ")}}]),e}(i.Component);e.default=p}}]);
+//# sourceMappingURL=7.bc6617f7.chunk.js.map
